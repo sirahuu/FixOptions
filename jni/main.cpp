f37d444736cbdb6d24c90648f0c7477c1a3e9e61
sirahuu <huugetu0131@gmail.com>
2017-05-09 19:17:36 +0900

    1.1.0.9

---
+++ jni/main.cpp
@@ -0,0 +1,34 @@
+#include <jni.h>
+#include <dlfcn.h>
+#include <android/log.h>
+#include <stdlib.h>
+#include <Substrate.h>
+#include <string>
+
+#define LOG_TAG "FixOptions"
+#define LOG(...) ((void)__android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__))
+
+class AppPlatform{
+ public:
+ static std::string HOME_PATH;
+ static std::string SETTINGS_PATH;
+};
+class AppPlatform_android{
+ public:
+ std::string _getAppDataOptionsLocation();
+ std::string _getDefaultStoragePath();
+ std::string& getInternalStoragePath();
+ std::string& getSettingsPath();
+ std::string& getUserdataPath();
+};
+std::string (*_AppPlatform_android$_getAppDataOptionsLocation)(AppPlatform_android*);
+std::string AppPlatform_android$_getAppDataOptionsLocation(AppPlatform_android* app){
+std::string path=_AppPlatform_android$_getAppDataOptionsLocation(app);
+LOG("%s",(app->getUserdataPath()+AppPlatform::SETTINGS_PATH).c_str());
+return app->getUserdataPath()+AppPlatform::SETTINGS_PATH;
+};
+
+JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
+	MSHookFunction((void*)&AppPlatform_android::_getAppDataOptionsLocation,(void*)&AppPlatform_android$_getAppDataOptionsLocation,(void**)&_AppPlatform_android$_getAppDataOptionsLocation);
+	return JNI_VERSION_1_2;
+}
